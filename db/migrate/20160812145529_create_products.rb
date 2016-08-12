3bfff784cd1db0074fd5544a04aefbb778240ed4
Louiefigz <figueroa.aaron@gmail.com>
2016-08-12 11:00:55 -0400

    Initial application

---
+++ db/migrate/20160812145529_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price, default: 0
+      t.text :description
+      t.timestamps
+    end
+  end
+end
